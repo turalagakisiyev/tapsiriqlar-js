e32ae249e861c1d4f5aba8672cc9f5a907488405
turalagakisiyev <agakisiyevtural6@gmail.com>
2022-10-28 15:00:17 +0400

    tural

---
+++ intel.js
@@ -0,0 +1,85 @@
+// tapsiriq 1
+
+
+// const sentence = ['sense.', 'make', 'all', 'will', 'This'];
+
+
+// function reverseArray (sentence) {
+//     let arr = sentence.reverse()
+//     return arr
+
+// }
+// console.log(reverseArray(sentence));
+
+
+// tapsiriq 2
+
+// const aliens = ['Blorgous', 'Glamyx', 'Wegord', 'SpaceKing'];
+
+// function greetAliens(aliens) {
+
+//     aliens.forEach((item) => {
+//         console.log("Oh powerful " + item + ", we humans offer our unconditional surrender")
+
+//     })
+
+// }
+// greetAliens(aliens);
+
+// tapsiriq 3
+
+// const coolStuff = [
+//   'gameboys',
+//   'skateboards',
+//   'backwards hats',
+//   'my room',
+//   'temporary tattoos'
+// ];
+// const myStuff = [
+//   'sweaters',
+//   'skateboards',
+//   'family-night',
+//   'my room',
+// ];
+// function justCoolStuff (coolStuff,myStuff){
+//     coolStuff.forEach((item)=>{
+//         myStuff.forEach((element)=>{
+//             if (item==element){
+//                 console.log(item)
+//             }
+//         })
+//     })
+
+// }
+// justCoolStuff(myStuff, coolStuff)
+
+
+// tapsiriq 4
+
+// const meal = [
+//     { name: 'arugula', source: 'plant' },
+//     { name: 'tomatoes', source: 'plant' },
+//     { name: 'lemon', source: 'plant' },
+//     { name: 'olive oil', source: 'plant' },
+// ];
+// const dinner = [
+//     { name: 'hamburger', source: 'meat' },
+//     { name: 'cheese', source: 'dairy' },
+//     { name: 'ketchup', source: 'plant' },
+//     { name: 'bun', source: 'plant' }
+// ];
+
+// function isTheDinnerVegan(arr) {
+
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i].source !== 'meat') {
+//             return false
+//         } else {
+//             return true
+//         }
+//     }
+// }
+
+// console.log(isTheDinnerVegan(meal));
+// console.log(isTheDinnerVegan(dinner));
